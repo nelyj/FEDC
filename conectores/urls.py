afde5a00a2178e0c6fea9e81d8435d52bec97ecb
Luis Barrios <nikeven@gmail.com>
2019-01-29 13:52:46 -0400

    generador CRUD para companias, agregada tabla para multiples companias

--- conectores/urls.py
+++ conectores/urls.py
@@ -5,4 +5,7 @@ app_name = 'conectores'
 urlpatterns = [
     path('registrar-conector/', ConectorViews.as_view(), name='registrar_conector'),
     path('registrar-compania/', CompaniaViews.as_view(), name='registrar_compania'),
+    path('registrar_compania/update/<pk>/',CompaniaUpdate.as_view(), name='update_compania'),
+    path('registrar_compania/delete/<pk>/',CompaniaDelete.as_view(), name='delete_compania'),
+
 ]
