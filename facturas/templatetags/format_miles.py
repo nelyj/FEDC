6782155b39b23e66f98a2bcf00634318d3d1a2f7
Rodrigo <rudmanmrrod@gmail.com>
2019-03-21 15:14:46 -0400

    Creada funcionalidad para imprimir las facturas en pdf segun el formato SII

---
+++ facturas/templatetags/format_miles.py
@@ -0,0 +1,19 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='format_mil')
+def formatSeparadorMiles(value, separador = '.'):
+    """
+    """
+    cadena = float(value)
+    if separador == '.':
+        lista_decimal = "{0:,}".format(cadena).replace(',','.').split('.')[-1:]
+        lista_num = "{0:,}".format(cadena).replace(',','.').split('.')[:-1]
+        return '.'.join(lista_num)+","+ lista_decimal[0]
+    elif separador == ',':
+        return "{0:,}".format(cadena)
+    else:
+        lista_decimal = "{0:,}".format(cadena).replace(',',separador).split('.')[-1:]
+        lista_num = "{0:,}".format(cadena).replace(',', separador).split('.')[:-1]
+        return '.'.join(lista_num)+","+ lista_decimal[0]
