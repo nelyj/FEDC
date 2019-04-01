a69ca604761d2126374cad176fac01bca05d6fb1
Administrator <arincones@vikua.com>
2019-04-01 19:35:39 -0400

    trabajando en modulo de intercambio

--- intercambios/views.py
+++ intercambios/views.py
@@ -1,7 +1,38 @@
+import imaplib
+
 from django.views.generic.base import TemplateView
-# Create your views here.
+from conectores.models import Compania
 
 
 class IntercambiosListView(TemplateView):
+	template_name = "intercambios.html"
+	
 
-	pass
+	def get_context_data(self, *args, **kwargs):
+
+		mail = imaplib.IMAP4_SSL('imap.gmail.com')
+		user = self.request.user
+		compania = Compania.objects.filter(owner=user).first()
+		print(compania)
+		assert compania.correo_intercambio, "No hay correo"
+		assert compania.pass_correo_intercambio, "N o hay password"
+		correo = compania.correo_intercambio.strip()
+		passw = compania.pass_correo_intercambio.strip()
+		print(correo,passw)
+		mail.login('code4road@gmail.com', 'gmaiSeneca88+')
+		mail.list()
+		mail.select("inbox")
+		result, data = mail.search(None, "ALL")
+		id_list = data[0].split()
+		latest_email = id_list[-1]
+		result, data = mail.fetch(latest_email, "(RFC822)")
+		lines = data[0]
+		print(lines[0])
+		print('//////////////////')
+		print(lines[1].decode())
+
+		return 
+
+# resources 
+# https://yuji.wordpress.com/2011/06/22/python-imaplib-imap-example-with-gmail/
+# https://docs.python.org/2/library/imaplib.html#imap4-objects
