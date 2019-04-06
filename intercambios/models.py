d33ed79b1e6c7e8c1d00b85a96b4f6c02286b428
Administrator <arincones@vikua.com>
2019-04-06 14:38:02 -0400

    creando funcion para actualizar bandeja de intercambio con el servidor de correos

--- intercambios/models.py
+++ intercambios/models.py
@@ -1,16 +1,22 @@
 from django.db import models
 
 from mixins.models import CreationModificationDateMixin
+from conectores.models import Compania
 # Create your models here.
 
 
 class Intercambio(CreationModificationDateMixin):
-	pass
 
-	# receptora 
-	# remitente 
-	# fecha_de_recepcion
-	# cantidad_dte
-	# titulo
-	# contenido
-	# dte
+	codigo_email = models.IntegerField(null=True, default=0)
+	receptor = models.ForeignKey(Compania, on_delete=models.CASCADE, blank=True, null=True)
+	remisor = models.CharField(max_length=128,blank=True, null=True)
+	email_remisor = models.EmailField(blank=True, null=True)
+	fecha_de_recepcion = models.CharField(max_length=128,blank=True, null=True)
+	cantidad_dte = models.IntegerField(null=True, default=0)
+	titulo = models.CharField(max_length=128,blank=True, null=True)
+	contenido = models.TextField(null=True, blank=True)
+
+class DteIntercambio(models.Model):
+
+	id_intercambio = models.ForeignKey(Intercambio, on_delete=models.CASCADE, blank=True, null=True)
+	dte = models.TextField(null=True, blank=True)
--- intercambios/views.py
+++ intercambios/views.py
@@ -3,14 +3,85 @@ from base64 import b64decode,b64encode
 
 from django.conf import settings
 
-from django.views.generic.base import TemplateView
+from django.views.generic import TemplateView, RedirectView, ListView
+from django.shortcuts import get_object_or_404
 from conectores.models import Compania
+from .models import Intercambio, DteIntercambio
 
 
-class IntercambiosListView(TemplateView):
+class IntercambiosListView(ListView):
 	template_name = "intercambios.html"
-	attachment_dir = settings.MEDIA_ROOT +'intercambio/'
 	
+	def get_queryset(self):
+
+		user = self.request.user
+		compania = get_object_or_404(Compania, owner=user)
+
+		return Intercambio.objects.filter(receptor=compania)
+
+
+class RefrescarBandejaRedirectView(RedirectView):
+
+	def get_redirect_url(self, *args, **kwargs):
+
+		mail = imaplib.IMAP4_SSL('imap.gmail.com')
+		user = self.request.user
+		compania = Compania.objects.filter(owner=user).first()
+		print(compania)
+		assert compania.correo_intercambio, "No hay correo"
+		assert compania.pass_correo_intercambio, "No hay password"
+		correo = compania.correo_intercambio.strip()
+		passw = compania.pass_correo_intercambio.strip()
+		print(correo,passw)
+		mail.login(compania.correo_intercambio,compania.pass_correo_intercambio)
+		mail.list()
+		print(mail.list())
+		mail.select("inbox")
+
+		result, data = mail.search(None, "ALL")
+		# print(self.get_emails(data, mail))
+
+		# Obtiene una lista de id de todos los emails
+		id_list = data[0].split()
+		# Obtiene el ultimo email
+		latest_emails = id_list[-20:]
+		email_list = []
+		for element in latest_emails:
+
+		
+			result, email_data = mail.fetch(element, "(RFC822)")
+			# Obtiene el binario de los emails indicados
+			raw_email = email_data[0][1]
+			raw_multipart = email.message_from_bytes(raw_email)
+			raw_email_string = raw_email.decode('utf-8')
+			email_message = email.message_from_string(raw_email_string)
+			attachment_count, attachments = self.get_attachment(raw_multipart)
+
+			# Intercambios.objects.create(
+			# 	codigo_email = element.decode(),
+			# 	receptor = 
+			# 	remisor = 
+			# 	email_remisor = 
+			# 	fecha_de_recepcion = 
+			# 	cantidad_dte = 
+			# 	titulo = 
+			# 	contenido 
+			# ) 
+
+			email_list.append(dict(
+				codigo=element.decode(),
+				from_email=str(email.header.make_header(email.header.decode_header(email_message['From']))),
+				subject=str(email.header.make_header(email.header.decode_header(email_message['Subject']))),
+				received=str(email.header.make_header(email.header.decode_header(email_message['Received']))),
+				body=self.get_body(raw_multipart).decode(),
+				attachment_count=attachment_count,
+				attachments=attachments
+
+			))
+
+		return reverse_lazy('intercambios:list')
+
+
 
 	def search(self, key, value, con):
 
@@ -43,7 +114,6 @@ class IntercambiosListView(TemplateView):
 				continue
 			if part.get('Content-Disposition') is None:
 				continue
-			print()
 			attachment_count += 1
 
 			fileName = part.get_filename()
@@ -52,109 +122,10 @@ class IntercambiosListView(TemplateView):
 			attachment_dict[decoded_filename] = part.get_payload(decode=True)
 
 		return (attachment_count,attachment_dict)
-			# print(fileName)
-			# if bool(fileName):
-			# 	filePath = os.path.join(self.attachment_dir, fileName)
-			# 	with open(filePath, 'wb') as f:
-			# 		f.write(part.get_payload(decode=True))
-			# 	print('Done!!')
-
-
-	def get_context_data(self, *args, **kwargs):
-
-		context = super().get_context_data(*args, **kwargs)
-		mail = imaplib.IMAP4_SSL('imap.gmail.com')
-		user = self.request.user
-		compania = Compania.objects.filter(owner=user).first()
-		print(compania)
-		assert compania.correo_intercambio, "No hay correo"
-		assert compania.pass_correo_intercambio, "No hay password"
-		correo = compania.correo_intercambio.strip()
-		passw = compania.pass_correo_intercambio.strip()
-		print(correo,passw)
-		mail.login(compania.correo_intercambio,compania.pass_correo_intercambio)
-		mail.list()
-		print(mail.list())
-
-
-		mail.select("inbox")
-
-		result, data = mail.search(None, "ALL")
-		# print(self.get_emails(data, mail))
-
-		# Obtiene una lista de id de todos los emails
-		id_list = data[0].split()
-		# Obtiene el ultimo email
-		latest_emails = id_list[-20:]
-		email_list = []
-		for element in latest_emails:
-
-		
-			result, email_data = mail.fetch(element, "(RFC822)")
-		# Obtiene el binario de los emails indicados
-
-			raw_email = email_data[0][1]
-			raw_multipart = email.message_from_bytes(raw_email)
-			raw_email_string = raw_email.decode('utf-8')
-			email_message = email.message_from_string(raw_email_string)
-			attachment_count, attachments = self.get_attachment(raw_multipart)
-
-			email_list.append(dict(
-				codigo=element.decode(),
-				from_email=str(email.header.make_header(email.header.decode_header(email_message['From']))),
-				subject=str(email.header.make_header(email.header.decode_header(email_message['Subject']))),
-				received=str(email.header.make_header(email.header.decode_header(email_message['Received']))),
-				body=self.get_body(raw_multipart).decode(),
-				attachment_count=attachment_count,
-				attachments=attachments
-
-			))
-
-		# for element in email_list:
-
-		# 	print(json.dumps(element, sort_keys=True))
-		context['lista_de_emails'] = email_list
-		return context
 
 
 
 
-
-
-
-		# email_from = str(email.header.make_header(email.header.decode_header(email_message['From'])))
-		# email_to = str(email.header.make_header(email.header.decode_header(email_message['To'])))
-		# subject = str(email.header.make_header(email.header.decode_header(email_message['Subject'])))
-		# print(from_email, subject, received)
-
-
-		# raw = email.message_from_bytes(data[0][1])
-		# self.get_attachment(raw)
-
-
-		# body = self.get_body(raw)
-		# print(body.decode())
-		# msgs = self.get_emails(self.search('FROM', 'no-reply@leetssadcode.com',mail), mail)
-		# for msg in msgs:
-
-		# 	print(self.get_body(email.message_from_bytes(msg[0][1])).decode())
-
-		# try:
-		    
-		#     # if(not os.path.isdir(attachment_dir)):
-		#     #     os.makedirs(settings.MEDIA_ROOT +'facturas'+'/'+self.kwargs['slug'])
-		#     f = open(xml_dir+'/'+self.kwargs['slug']+'.xml','w')
-		#     f.write(documento_final_firmado)
-		#     f.close()
-		# except Exception as e:
-		#     messages.error(self.request, 'Ocurrio el siguiente Error: '+str(e))
-		#     return super().form_valid(form)
-		# lines = data[0]
-		# print(lines[0])
-		# print('//////////////////')
-		# print(lines[1].decode())
-
-		return 
 
 # resources 
 # https://yuji.wordpress.com/2011/06/22/python-imaplib-imap-example-with-gmail/
