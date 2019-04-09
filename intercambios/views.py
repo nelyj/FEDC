b23a11fb57a2a415ce32ca3734fa3c71f7208db3
Administrator <arincones@vikua.com>
2019-04-09 14:07:37 -0400

    modulo de intercambio, capturando errores de conexion con imap y corrigiendo errores de formato en las variables de salida

--- intercambios/views.py
+++ intercambios/views.py
@@ -1,4 +1,5 @@
 import imaplib, email, os, json
+
 from datetime import datetime
 from base64 import b64decode,b64encode
 
@@ -67,54 +68,50 @@ class RefrescarBandejaRedirectView(RedirectView):
 		correo = compania.correo_intercambio.strip()
 		passw = compania.pass_correo_intercambio.strip()
 		print(correo,passw)
-		mail.login(compania.correo_intercambio,compania.pass_correo_intercambio)
+		try:
+
+			mail.login(correo,passw)
+		except imaplib.IMAP4.error as e:
+			
+			messages.error(self.request, str(e))
+			return reverse_lazy('intercambios:lista', kwargs={'pk':pk})
 		mail.list()
-		print(mail.list())
 		mail.select("inbox")
-
 		result, data = mail.search(None, "ALL")
-		# print(self.get_emails(data, mail))
-
-		# Obtiene una lista de id de todos los emails
-
 		id_list = data[0].split()
-
 		try:
 			last_email = Intercambio.objects.latest('codigo_email')
 		except Intercambio.DoesNotExist:
 			last_email = 0
 
-		if int(id_list[-1].decode()) > int(last_email.codigo_email):
+		last_email_code = int(id_list[-1].decode())
+		if last_email == 0:
 
-			new_elements = int(id_list[-1].decode()) - int(last_email.codigo_email)
+			local_last_email_code = last_email
+		else:
 
+			local_last_email_code = int(last_email.codigo_email)
+		if last_email_code > local_last_email_code:
+
+			new_elements = last_email_code - local_last_email_code
 		else:
 			messages.info(self.request, "No posee nuevos correos")
 			return reverse_lazy('intercambios:lista', kwargs={'pk':pk})
+		if new_elements == 1:
 
+			latest_emails = [id_list[-1]]
+		else:
 
-		# if id_list[-1].decode() > Intercambio.objects.get(receptor=compania).order_by('-codigo_email').
-
-
-		# Obtiene el ultimo email
-		# if new_elements == 1:
-		# 	latest_emails = [id_list[-new_elements]]
-		# else:
-		latest_emails = id_list[-new_elements:]
-
-		email_list = []
+			latest_emails = id_list[-new_elements:]
 		for element in latest_emails:
 
-		
 			result, email_data = mail.fetch(element, "(RFC822)")
-			# Obtiene el binario de los emails indicados
 			raw_email = email_data[0][1]
 			raw_multipart = email.message_from_bytes(raw_email)
 			raw_email_string = raw_email.decode('utf-8')
 			email_message = email.message_from_string(raw_email_string)
 			attachment_count, attachments = self.get_attachment(raw_multipart)
 			remisor_name, remisor_email = self.get_remisor(str(email.header.make_header(email.header.decode_header(email_message['From']))))
-
 			Intercambio.objects.create(
 				codigo_email = element.decode(),
 				receptor = compania,
@@ -126,26 +123,8 @@ class RefrescarBandejaRedirectView(RedirectView):
 				contenido = self.get_body(raw_multipart).decode('utf-8')
 			) 
 
-			# email_list.append(dict(
-			# 	codigo=element.decode(),
-			# 	remisor=remisor_name,
-			# 	email_remisor=remisor_email,
-			# 	subject=str(email.header.make_header(email.header.decode_header(email_message['Subject']))),
-			# 	received=self.get_received_time(str(email.header.make_header(email.header.decode_header(email_message['Received'])))),
-			# 	body=self.get_body(email_message).decode('utf-8'),
-			# 	attachment_count=attachment_count,
-			# 	attachments=attachments
-
-			# ))
-
-
-		# for item in email_list:
-
-		# 	print(item['body'])
 		messages.success(self.request, "Cantidad de correos nuevos: {}".format(new_elements))
 		return reverse_lazy('intercambios:lista', kwargs={'pk':pk})
-
-
 
 	def search(self, key, value, con):
 
@@ -179,22 +158,19 @@ class RefrescarBandejaRedirectView(RedirectView):
 			if part.get('Content-Disposition') is None:
 				continue
 			attachment_count += 1
-
 			fileName = part.get_filename()
-			decoded_filename = str(email.header.make_header(email.header.decode_header(part.get_filename())))
-
-			attachment_dict[decoded_filename] = part.get_payload(decode=True)
-
+			if fileName is not None:
+				decoded_filename = str(email.header.make_header(email.header.decode_header(fileName)))
+				attachment_dict[decoded_filename] = part.get_payload(decode=True)
+			else:
+				attachment_dict["dte"+str(attachment_count)] = part.get_payload(decode=True)
 		return (attachment_count,attachment_dict)
 
 	def get_received_time(self, received_string):
 
 		list_ = received_string.split('\r\n')
-
 		final_date = list_[1].split()
-
 		string_date = "{} {} {}".format(final_date[2], final_date[1], final_date[3])
-
 		datef = datetime.strptime(string_date, '%b %d %Y')
 
 		return datef
@@ -202,25 +178,12 @@ class RefrescarBandejaRedirectView(RedirectView):
 	def get_remisor(self, remisor_string):
 
 		remisor = remisor_string.split('<')
-
-		remisor_mail = remisor[1].strip('<>')
-		remisor_name = remisor[0].strip()
-
-		return remisor_name, remisor_mail
-
-
-
-# Mon, 8 Apr 2019 06:03:09 -0700 (PDT)
-# 'Jun 1 2005  1:33PM'
-
-
-# resources 
-# https://yuji.wordpress.com/2011/06/22/python-imaplib-imap-example-with-gmail/
-# https://docs.python.org/2/library/imaplib.html#imap4-objects
-# https://rauth.readthedocs.io/en/latest/api/
-# https://developers.google.com/gmail/imap/xoauth2-protocol
+		if len(remisor) > 1: 
+			remisor_mail = remisor[1].strip('<>')
+			remisor_name = remisor[0].strip()
+			return remisor_name, remisor_mail
+		else:
+			return "Desconocido", remisor[0]
 
 
 
-# email_message keys:
-# ['Delivered-To', 'Received', 'X-Google-Smtp-Source', 'X-Received', 'ARC-Seal', 'ARC-Message-Signature', 'ARC-Authentication-Results', 'Return-Path', 'Received', 'Received-SPF', 'Authentication-Results', 'DKIM-Signature', 'DKIM-Signature', 'Received', 'Received', 'Content-Type', 'Date', 'From', 'Mime-Version', 'To', 'Message-ID', 'Subject', 'X-SG-EID', 'X-SG-ID', 'X-Feedback-ID']
