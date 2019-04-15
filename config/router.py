bde1d2b2f954d819f6f0307db54b57a219537f0d
Rodrigo <rodrigoale.b@timg.cl>
2019-04-15 10:43:07 -0400

    Añadido rest framework, agregada data inicial, añadido a los requerimientos

---
+++ config/router.py
@@ -0,0 +1,13 @@
+"""
+Facturador TIMG
+@package config.router
+
+Router del api de Rest Framework
+@copyright TIMG
+@version 1.0
+"""
+from rest_framework import routers
+from facturas.viewset import *
+
+router = routers.DefaultRouter()
+router.register(r'facturas', FacturaViewSet, basename='facturas')
