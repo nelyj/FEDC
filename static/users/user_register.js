e0ffa1b79aa51d242bcefc0b51880133b0aedbe5
rodrigoaleb <rodrigoale.b@timg.cl>
2020-01-03 12:41:50 -0400

    Se optimizo la creacion de usuario del sistema, llegara un correo al usuario creado desde el administrador

--- static/users/user_register.js
+++ static/users/user_register.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
     $("#id_is_staff").on("change", function(){
         if ($('#id_is_staff').is(':checked')){
@@ -11,13 +12,45 @@ $(document).ready(function() {
             });   
         }
     });
+    
+    $('#show').click(function(){
+        $('#show').removeClass( "glyphicon glyphicon-eye-open" ).addClass( "glyphicon glyphicon-eye-close" )
+        if (!$("#check").is(':checked')){
+                $("#check").prop("checked", true)
+                $("#check").attr('checked', 'checked')
+                $("#check").is(':checked') ? $('#id_password1').attr('type', 'text') : $('#id_password1').attr('type', 'password');
+        }
+        else{
+            $('#show').removeClass( "glyphicon glyphicon-eye-close" ).addClass( "glyphicon glyphicon-eye-open" )
+            $("#check").prop("checked", false)
+            $("#check").is(':checked') ? $('#id_password1').attr('type', 'text') : $('#id_password1').attr('type', 'password');
+        }
+    });
+
     $('#meter').entropizer({
-                target: '#id_password1',
-                update: function(data, ui) {
-                    ui.bar.css({
-                        'background-color': data.color,
-                        'width': data.percent + '%'
-                    });
-                }
+        target: '#id_password1',
+        update: function(data, ui) {
+            ui.bar.css({
+                'background-color': data.color,
+                'width': data.percent + '%'
             });
+        }
+    });
 });
+
+function ramdomString(length){
+        var password = Math.random().toString(36).substring(2, length) + Math.random().toString(36).substring(2, length);
+        $('#id_password1').val(password)
+        $('#id_password2').val(password)
+        $('#meter').entropizer('destroy');
+        $('#meter').entropizer({
+            target: '#id_password1',
+            update: function(data, ui) {
+                ui.bar.css({
+                    'background-color': data.color,
+                    'width': data.percent + '%'
+                });
+            }
+        });
+        return password;
+};
